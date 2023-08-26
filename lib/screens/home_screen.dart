28a641c673638ae5d9b20d55f39df51be7dcabb4
WhiteKr <hanjisu2215g@gmail.com>
2023-08-27 03:23:32 +0900

    fix: wrong twentyFiveMinutes seconds

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 5;
+  static const twentyFiveMinutes = 1500;
 
   int totalSeconds = twentyFiveMinutes;
   bool isRunning = false;
